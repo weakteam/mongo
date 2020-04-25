43ab8b11ae85ac51175e7c3b5f1a9145596e2266
Aristov Ivan <apostrof1995@gmail.com>
2020-04-25 22:09:22 +0300

    KeyWriter #28

---
+++ core/src/main/scala/io/github/weakteam/mongo/bson/BsonKeyWriter.scala
@@ -0,0 +1,25 @@
+package io.github.weakteam.mongo.bson
+
+import simulacrum.typeclass
+import io.github.weakteam.mongo.bson.BsonWriter.nonInheritedOps.toBsonWriterOps
+
+@typeclass
+trait BsonKeyWriter[-A] {
+  def writeKey(key: A): String
+}
+
+object BsonKeyWriter {
+  private[bson] final class KeyWriterOps(private val key: String) extends AnyVal {
+    def :=[B: BsonWriter](value: B): (String, BsonValue) = {
+      (key, value.writeBson)
+    }
+  }
+
+  trait KeyWriterSyntax {
+    implicit def toKeyWriterOps[A](key: A)(implicit wr: BsonKeyWriter[A]): KeyWriterOps =
+      new KeyWriterOps(wr.writeKey(key))
+  }
+
+  object KeyWriterSyntax extends KeyWriterSyntax
+
+}
