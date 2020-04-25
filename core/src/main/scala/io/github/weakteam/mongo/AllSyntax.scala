2099d59b1dacaec6926373a0954083b3d64654ba
Aristov Ivan <apostrof1995@gmail.com>
2020-04-25 22:09:22 +0300

    KeyWriter #28

--- core/src/main/scala/io/github/weakteam/mongo/AllSyntax.scala
+++ core/src/main/scala/io/github/weakteam/mongo/AllSyntax.scala
@@ -1,5 +1,9 @@
 package io.github.weakteam.mongo
 
-import io.github.weakteam.mongo.bson.BsonWriter
+import io.github.weakteam.mongo.bson.{BsonKeyWriter, BsonReader, BsonWriter}
 
-trait AllSyntax extends BsonWriter.ToBsonWriterOps
+trait AllSyntax
+  extends BsonWriter.ToBsonWriterOps
+  with BsonKeyWriter.ToBsonKeyWriterOps
+  with BsonKeyWriter.KeyWriterSyntax
+  with BsonReader.ToBsonReaderOps
